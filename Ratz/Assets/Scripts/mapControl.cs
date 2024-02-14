550cd959534fb2bc7228f26a53eef4539b062579
spaZzZyyy <soch5495@colorado.edu>
2024-02-14 09:19:54 -0700

    rig

--- Ratz/Assets/Scripts/mapControl.cs
+++ Ratz/Assets/Scripts/mapControl.cs
@@ -9,57 +9,66 @@ public class mapControl : MonoBehaviour
     [SerializeField] musicManager musicManager;
     float trackSpeed;
     int trackTime;
-    bool canChangeSpeed = true;
+    //bool canChangeSpeed = true;
+    bool musicPaused = false;
+    int trackSpeedMax = 5;
+    int trackSpeedMin = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         ani = GetComponent<Animator>();
         trackTime = 3;
+        trackSpeed = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Track Speed: " + trackSpeed);
+        Debug.Log("Track time: " + trackTime);
+        if (musicPaused == false){
+            ani.speed = trackSpeed;
+        }
         switch(trackTime){
             case 5:
-                trackSpeed = 6;
+                trackSpeed = 5f;
             break;
             case 4:
-                trackSpeed = 3;
+                trackSpeed = 3f;
             break;
             case 3:
-                trackSpeed = 1;
+                trackSpeed = 1f;
             break;
             case 2:
-                trackSpeed = 0.2f;
+                trackSpeed = 0.5f;
             break;
 
             default: // case 1
-                trackSpeed = 0f;
+                trackSpeed = 0.1f;
             break;
         }
-        ani.speed = trackSpeed;
+        
 
         if (Input.GetKeyDown(scriptControls.musicStartStop)){ // Pause Music
             ani.speed = 0;
+            musicPaused = true;
         }
         if (Input.GetKeyUp(scriptControls.musicStartStop)){ // UnPause Music
             ani.speed = trackSpeed;
+            musicPaused = false;
         }
 
-        if(trackTime > 1 && trackTime < 5){
-            canChangeSpeed = true;
-        }else {
-            canChangeSpeed = false;
+        if(Input.GetKeyDown(scriptControls.pitchUp)){
+            if(trackTime < trackSpeedMax){
+                trackTime += 1;
+            }
         }
 
-        if(Input.GetKeyDown(scriptControls.pitchUp) && canChangeSpeed == true){
-            trackTime += 1;
-        }
-
-        if(Input.GetKeyDown(scriptControls.pitchDown) && canChangeSpeed == true){
-            trackTime -= 1;
+        if(Input.GetKeyDown(scriptControls.pitchDown)){
+            if(trackTime > trackSpeedMin){
+                trackTime -= 1;
+            }
         }
     }
 }
