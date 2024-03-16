17b06e012b031e1c77b8bed43df8c76895624dde
Elirach <chne9912@colorado.edu>
2024-03-15 21:41:48 -0600

    dash animation fixed

--- Ratz/Assets/destroyPlatform.cs
+++ Ratz/Assets/destroyPlatform.cs
@@ -6,7 +6,6 @@ public class destroyPlatform : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.gameObject.name);
         if (collision.gameObject.name == "SinePlatform(Clone)")
         {
             Destroy(collision.gameObject);
