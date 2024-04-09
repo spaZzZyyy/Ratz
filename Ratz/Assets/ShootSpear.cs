de74f4bb9c2746715eaa1b2d5052594c6d9cb4ab
spaZzZyyy <soch5495@colorado.edu>
2024-04-09 07:17:21 -0600

    floor

--- Ratz/Assets/ShootSpear.cs
+++ Ratz/Assets/ShootSpear.cs
@@ -13,22 +13,13 @@ public class ShootSpear : MonoBehaviour
         var parents = GameObject.FindGameObjectsWithTag("spearSpawn");
         for (int i = 0; i < parents.Length; i++)
         {
-            var parent = parents[i];  
+            var parent = parents[i];
             if (parent.transform.childCount == 0)
             {
                 var newPlatform = Instantiate(Resources.Load("Prefabs/Platforms/spear"), parent.transform);
                 newPlatform.GetComponent<spears>().direction = parent.gameObject.GetComponent<ShootSpear>().direction;
                 newPlatform.GetComponent<spears>().speed = parent.gameObject.GetComponent<ShootSpear>().speed;
             }
-        }
-        
-        if (collision.gameObject.tag == "wall")
-        {
-            gameObject.tag = "platform";
-            rb.isKinematic = true;
-            transform.Rotate(0,0,0);
-            fadeTimer = destroyTime;
-            
         }
     }
 }
